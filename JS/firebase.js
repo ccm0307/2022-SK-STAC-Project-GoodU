ddeaa575a24b31982e261a43d511d0c55590d213
최창민 <changmin03074@gmail.com>
2022-09-19 19:23:42 +0900

    firebaseConfig

---
+++ JS/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB688FRjdT9kWdiZ6jBuQ1a29AcW81lWBw",
+    authDomain: "goodu-f6bfa.firebaseapp.com",
+    projectId: "goodu-f6bfa",
+    storageBucket: "goodu-f6bfa.appspot.com",
+    messagingSenderId: "513922668749",
+    appId: "1:513922668749:web:8a4dee0f6637a91acc3184"
+};
+  
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
