29e9fa7c206db70ad679a6a86450823e51213392
최창민 <changmin03074@gmail.com>
2022-09-19 19:30:45 +0900

    googleLogin

--- JS/firebase.js
+++ JS/firebase.js
@@ -1,11 +1,47 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB688FRjdT9kWdiZ6jBuQ1a29AcW81lWBw",
-    authDomain: "goodu-f6bfa.firebaseapp.com",
-    projectId: "goodu-f6bfa",
-    storageBucket: "goodu-f6bfa.appspot.com",
-    messagingSenderId: "513922668749",
-    appId: "1:513922668749:web:8a4dee0f6637a91acc3184"
+  apiKey: "AIzaSyB688FRjdT9kWdiZ6jBuQ1a29AcW81lWBw",
+  authDomain: "goodu-f6bfa.firebaseapp.com",
+  projectId: "goodu-f6bfa",
+  storageBucket: "goodu-f6bfa.appspot.com",
+  messagingSenderId: "513922668749",
+  appId: "1:513922668749:web:8a4dee0f6637a91acc3184",
 };
-  
+
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+$(document).ready(function ($) {
+  // 로그인 콜백
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      userLogin = true;
+      console.log(user);
+      $(".profile_name").html(user.displayName); // 이름 영역에 유저이름 띄우기
+      userName = user.displayName;
+      userEmail = user.email;
+      userDoc = user.uid;
+      console.log(userDoc); // 특정 user document 읽어오기
+    } else {
+      userLogin = false;
+      console.log("not login");
+      $(".profile_name").text("");
+    }
+  });
+});
+
+/**
+ * 로그아웃
+ */
+function gLogOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(
+      function () {
+        console.log("LogOut Success");
+      },
+      function (error) {
+        console.log("logOut Failed");
+      }
+    );
+}
--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,66 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB688FRjdT9kWdiZ6jBuQ1a29AcW81lWBw",
-    authDomain: "goodu-f6bfa.firebaseapp.com",
-    projectId: "goodu-f6bfa",
-    storageBucket: "goodu-f6bfa.appspot.com",
-    messagingSenderId: "513922668749",
-    appId: "1:513922668749:web:8a4dee0f6637a91acc3184"
+  apiKey: "AIzaSyB688FRjdT9kWdiZ6jBuQ1a29AcW81lWBw",
+  authDomain: "goodu-f6bfa.firebaseapp.com",
+  projectId: "goodu-f6bfa",
+  storageBucket: "goodu-f6bfa.appspot.com",
+  messagingSenderId: "513922668749",
+  appId: "1:513922668749:web:8a4dee0f6637a91acc3184",
 };
-  
+
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+$(document).ready(function ($) {
+  // 로그인 콜백
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log(user);
+      $(".profile_name").text(user.displayName); // 사용자명 표시
+      location.href = "/main.html"; // 메인화면 이동
+    } else {
+      console.log("log out");
+      $(".profile_name").text("");
+    }
+  });
+});
+
+/**
+ * 구글 로그인
+ */
+function googleLogin() {
+  // 구글 소셜 로그인
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope("https://www.googleapis.com/auth/plus.login");
+  provider.setCustomParameters({
+    prompt: "select_account",
+  });
+  firebase
+    .auth()
+    .signInWithRedirect(provider)
+    .then(function (result) {
+      firebase
+        .auth()
+        .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+        .then(() => {
+          var token = result.credential.accessToken;
+          var user = result.user;
+        })
+        .catch(function (error) {
+          var errorCode = error.code;
+          var email = error.email;
+        });
+    });
+}
+
+/**
+ * 로그아웃
+ */
+function logOut() {
+  // 로그아웃
+  firebase
+    .auth()
+    .signOut()
+    .then(
+      function () {},
+      function (error) {}
+    );
+}
